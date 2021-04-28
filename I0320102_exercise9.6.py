baf77b25ee1ef20398758ccac8efb32bd707a838
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-28 22:58:10 +0700

    Exercise 9.6

--- I0320102_exercise9.6.py
+++ I0320102_exercise9.6.py
@@ -1,1 +1,24 @@
+import array
+A = array.array('i')
 
+A.append(50)
+print(A)
+A.append(-20)
+print(A)
+A.append(30)
+print(A)
+A.insert(1,40)
+print(A)
+A.remove(-20)
+print(A)
+
+#menggunakan perintah for
+for nilai in A :
+    print("%d " % nilai, end='')
+
+#menggunakan perintah while
+i = 0
+while i < len(A):
+    print("%d " % A[i], end='')
+    i += 1
+    A.index(30)
