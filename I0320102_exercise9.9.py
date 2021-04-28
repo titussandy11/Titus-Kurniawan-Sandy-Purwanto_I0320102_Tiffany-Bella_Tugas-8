f77c78aff5771b171d0f4e804fe24461fa53c487
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-28 23:03:43 +0700

    Exercise 9.9

--- I0320102_exercise9.9.py
+++ I0320102_exercise9.9.py
@@ -1,1 +1,9 @@
+#mendefinisikan objek array dengan nilai awal tertentu
+import array
+A = array.array('i', [100,200,300,400,500])
+print(A)
 
+#mengubah nilai dari elemen tertentu
+A[1] = -700 #mengubah elemen kedua
+A[4] = 800  #mengubah elemen kelima
+print(A)
