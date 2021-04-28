8de35939fe5f4445d1546dbc2f9aab40c0e132d4
Titus Kurniawan Sandy <titussandy11@gmail.com>
2021-04-28 22:50:55 +0700

    Exercise 9.3

--- I0320102_exercise9.3.py
+++ I0320102_exercise9.3.py
@@ -1,1 +1,25 @@
+#mendefinisikan array dua dimensi
 
+A = [
+        [23,11,54,38,76],
+        [14,12,20,22,21],
+        [10,13,18,17,24],
+        [35,33,39,32,29]
+    ]
+baris = 4
+kolom = 5
+
+def main():
+    print('Isi array A: ')
+    # menampilkan elemen array dua dimensi
+    i = 0;
+    while i < baris:
+        j = 0
+        while j < kolom:
+            print('%d ' % A[i][j], end='')
+            j += 1
+        i += 1
+        print() #ganti baris
+
+if __name__ == '__main__':
+    main()
